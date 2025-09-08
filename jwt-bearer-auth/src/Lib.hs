db7b046efb2b06dd3fd925754cc45e44a0ef716f
Robert Benson <robert.benson@renaissance.com>
2025-09-08 16:25:38 -0400

    fix compiler errors and warnings. No orphan instance is needed for (MonadLogger (JOSE e m)) because we can simply use our own ExceptT and satisfy (MonadError e m, AsError e, AsJWTError e) by implementing the prisms

--- jwt-bearer-auth/src/Lib.hs
+++ jwt-bearer-auth/src/Lib.hs
@@ -14,7 +14,6 @@ import qualified Data.ByteString as BS
 import Data.Aeson
 import Data.Text.Encoding (decodeUtf8)
 import Control.Monad.Logger.Aeson
-import GHC.Stack (HasCallStack)
 import Control.Lens hiding ((.=))
 import Network.HTTP.Simple
 
@@ -28,17 +27,27 @@ verifyTokenClaims
   -> BS.ByteString
   -> ExceptT (AuthError JWTError) m ClaimsSet
 verifyTokenClaims tokenServerUrl token = do
-  joseToAuthError $ do
-    logInfo $ "decoding bearer token" :# ["token" .= decodeUtf8 token]
-    jwt <- decodeCompact $ BSL.fromStrict token
-    logInfo $ "decoded bearer token" :# ["jwt" .= jwt]
-    verifyClaims @_ @_ @JWTError
-      insecureJWTValidationSettings
-      tokenServerUrl
-      jwt
+  logInfo $ "decoding bearer token" :# ["token" .= decodeUtf8 token]
+  jwt <- decodeCompact $ BSL.fromStrict token
+  logInfo $ "decoded bearer token" :# ["jwt" .= jwt]
+  verifyClaims @_ @_ @(AuthError JWTError)
+    insecureJWTValidationSettings
+    tokenServerUrl
+    jwt
 
 data AuthError a = NoBearerToken | JOSEError a
   deriving stock (Generic, Show)
+
+_JOSEError :: Prism (AuthError a) (AuthError b) a b
+_JOSEError = prism JOSEError $ \case
+    NoBearerToken -> Left NoBearerToken
+    JOSEError x -> Right x
+
+instance AsJWTError a => AsJWTError (AuthError a) where
+  _JWTError = _JOSEError._JWTError
+
+instance AsError a => AsError (AuthError a) where
+  _Error = _JOSEError._Error
 
 newtype TokenServerUrl = TokenServerUrl {unTokenServerUrl :: String}
   deriving stock (Eq, Show)
@@ -48,10 +57,6 @@ newtype WellKnownJWKSet = WellKnownJWKSet
   }
   deriving stock (Generic, Show)
   deriving anyclass (FromJSON)
-
-
-joseToAuthError :: Functor m => JOSE e m a -> ExceptT (AuthError e) m a
-joseToAuthError = withExceptT JOSEError . unwrapJOSE
 
 instance
   ( HasKid h, MonadIO m, MonadLogger m)
@@ -69,17 +74,10 @@ instance
 -- Fetch JWKs from the token server
 -- in reality, this should be cached
 fetchJWKs
-  :: ( HasCallStack
-     , MonadIO m
-     -- , MonadLogger m
-     )
+  :: MonadIO m
   => TokenServerUrl
   -- ^ JWKs endpoint URL
   -> m WellKnownJWKSet
 fetchJWKs tokenServerUrl = do
   let request = parseRequest_ (unTokenServerUrl tokenServerUrl <> "/.well-known/jwks.json")
   getResponseBody <$> httpJSON request
-
-
--- necessary orphan instances
-deriving newtype instance MonadLogger m => MonadLogger (JOSE e m)
