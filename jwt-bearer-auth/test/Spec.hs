6eb64f58502b2ab2ec1f4b50f00e7445af6282c6
Restyled.io <commits@restyled.io>
2025-09-08 21:14:18 +0000

    Restyled by fourmolu

--- jwt-bearer-auth/test/Spec.hs
+++ jwt-bearer-auth/test/Spec.hs
@@ -1,16 +1,16 @@
 import Prelude
 
 import Control.Lens
-import Web.Auth.Bearer.JWT
 import Test.Hspec
+import Web.Auth.Bearer.JWT
 
 main :: IO ()
 main = hspec $ do
-  describe "_JOSEError prism" $
-    do
-      it "extracts the JOSEError if present" $
-        do
+  describe "_JOSEError prism"
+    $ do
+      it "extracts the JOSEError if present"
+        $ do
           JOSEError @String "Foo" ^? _JOSEError `shouldBe` Just "Foo"
-      it "returns Nothing if it wasn't a JOSEError" $
-        do
+      it "returns Nothing if it wasn't a JOSEError"
+        $ do
           NoBearerToken @String ^? _JOSEError `shouldBe` Nothing
