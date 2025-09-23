3a40e1c97cc299c18e02f7b4859bcfa8dcbac425
Robert Benson <robert.benson@renaissance.com>
2025-09-22 21:47:40 -0400

    make ClaimsWithScope more polymorphic. Not just because it feels cooler but because you might actually want to extend with several more claims

--- jwt-bearer-auth/src/Web/Auth/Bearer/JWT/Claims.hs
+++ jwt-bearer-auth/src/Web/Auth/Bearer/JWT/Claims.hs
@@ -3,7 +3,7 @@ module Web.Auth.Bearer.JWT.Claims
   ( ClaimsWithScope (..)
   , ClaimsSet
   , HasClaimsSet (..)
-  , claimScp
+  , HasScp (..)
   ) where
 
 import Prelude
@@ -15,28 +15,32 @@ import Data.Aeson.KeyMap
 -- | This "extends" the base '@ClaimsSet@' type by adding an additional "scp"
 --   claim. The ToJSON and FromJSON instances put the "scp" field alongside all
 --   the other fields in the object, not using a separate sub-object.
-data ClaimsWithScope = ClaimsWithScope ClaimsSet [String]
+data ClaimsWithScope a = ClaimsWithScope [String] a
   deriving stock (Show, Eq)
 
-instance FromJSON ClaimsWithScope where
+instance FromJSON a => FromJSON (ClaimsWithScope a) where
   parseJSON = withObject "ClaimsWithScope" $ \v ->
     ClaimsWithScope
-      <$> parseJSON @ClaimsSet (Object (delete "scp" v))
-      <*> (concat <$> v .:? "scp")
+      <$> (concat <$> v .:? "scp")
+      <*> parseJSON (Object (delete "scp" v))
 
-instance ToJSON ClaimsWithScope where
+instance ToJSON a => ToJSON (ClaimsWithScope a) where
   toJSON (ClaimsWithScope claims scp) = 
     let ~(Object o) = toJSON claims
      in Object $ insert "scp" (toJSON scp) o
 
-instance HasClaimsSet ClaimsWithScope where
-  claimsSet = lens getter setter
+instance HasClaimsSet a => HasClaimsSet (ClaimsWithScope a) where
+  claimsSet = myClaimsSet . claimsSet
     where
-      getter (ClaimsWithScope claims _) = claims
-      setter (ClaimsWithScope _ scp) claims = ClaimsWithScope claims scp
+      myClaimsSet = lens getter setter
+      getter (ClaimsWithScope _ claims) = claims
+      setter (ClaimsWithScope scp _) claims = ClaimsWithScope scp claims
 
-claimScp :: Lens' ClaimsWithScope [String]
-claimScp = lens getter setter
-  where
-    getter (ClaimsWithScope _ scp) = scp
-    setter (ClaimsWithScope claims _) scp = ClaimsWithScope claims scp
+class HasScp a where
+  claimScp :: Lens' a [String]
+
+instance HasScp (ClaimsWithScope a) where
+  claimScp = lens getter setter
+    where
+      getter (ClaimsWithScope scp _) = scp
+      setter (ClaimsWithScope _ claims) scp = ClaimsWithScope scp claims
